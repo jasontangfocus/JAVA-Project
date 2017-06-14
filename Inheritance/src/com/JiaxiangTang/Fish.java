6ee6c149c756f728ce3058f348c29bf0186d0fea
Jiaxiang Tang <jasontangfocus@gmail.com>
2017-06-13 20:40:39 -0500

    Inheritance Demo Comiitted 8:40pm 20170613

---
+++ Inheritance/src/com/JiaxiangTang/Fish.java
@@ -0,0 +1,38 @@
+package com.JiaxiangTang;
+
+/**
+ * Created by tjiax on 6/13/2017.
+ */
+public class Fish extends Animal {
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    private void rest() {
+
+    }
+
+    private void moveMuscles() {
+
+    }
+
+    private void moveBackFins() {
+
+    }
+
+    private void swim(int speed) {
+        moveMuscles();
+        moveBackFins();
+        super.move(speed);
+        
+    }
+
+}
