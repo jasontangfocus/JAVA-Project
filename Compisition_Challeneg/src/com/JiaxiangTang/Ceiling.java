5b5a68c9788c0c08db5449ef3b775fc3838d9768
jasontangfocus <jasontangfocus@gmail.com>
2017-06-16 10:49:48 -0500

    committed 10:49am 20170616

---
+++ Compisition_Challeneg/src/com/JiaxiangTang/Ceiling.java
@@ -0,0 +1,23 @@
+package com.JiaxiangTang;
+
+/**
+ * Created by jiaxiangtang on 6/16/17.
+ */
+public class Ceiling {
+
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
