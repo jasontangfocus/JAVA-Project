14bbe571e20f1400512ea7bc99d28145739d9bb3
jasontangfocus <jasontangfocus@gmail.com>
2017-08-19 20:33:08 -0500

    project 4

---
+++ Project4-SimpleBankingApplication/src/com/JohnT/Main.java
@@ -0,0 +1,19 @@
+package com.JohnT;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        * Create a simple banking application
+        * There should include a Bank Class
+        * It should have an arrayList of Branches
+        * Each branch should have an arrayList of customers
+        * The Customer class should have an arrayList of Doubles (transactions)
+        * Customer:
+        *   Name, and the ArrayList of doubles
+        *
+        * */
+
+    }
+}
