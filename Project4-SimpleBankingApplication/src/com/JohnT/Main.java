ce333cdacee3125f61cda5ba8a4da444b7cfd2f9
jasontangfocus <jasontangfocus@gmail.com>
2017-08-21 17:14:35 -0500

    project 4 half

--- Project4-SimpleBankingApplication/src/com/JohnT/Main.java
+++ Project4-SimpleBankingApplication/src/com/JohnT/Main.java
@@ -12,8 +12,22 @@ public class Main {
         * The Customer class should have an arrayList of Doubles (transactions)
         * Customer:
         *   Name, and the ArrayList of doubles
-        *
+        * Branch:
+        *   Need to be able to add a New customer and initial transaction amount
+        *   Also needs to add additional transactions for that customer/branch
+        * Bank:
+        *   Add a new branch
+        *   Add a customer to that branch with initial transaction
+        *   Show a list of customers for a particular branch and optionally a list of their transactions
+        * Demonstration autoboxing and unboxing in your code
+        * Hint: Transaction
+        * Add data validation
+        * e.g. check if exists, or does not exist etc
+        * Think about where you are adding the code to perform certain actions
         * */
+
+        
+
 
     }
 }
